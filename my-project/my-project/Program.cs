5e5e0d45b5f34f8aa9754a9cf340fff915564fee
KarimjonMurodov <61445376+KarimjonMurodov@users.noreply.github.com>
2023-06-24 15:34:43 +0500

    add
    matimatika amallari

--- my-project/my-project/Program.cs
+++ my-project/my-project/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Ishni boshlaymiz");
+
+            int a = 22, b = 11;
+
+            Console.WriteLine(a  + b);
+
         }
     }
 }
